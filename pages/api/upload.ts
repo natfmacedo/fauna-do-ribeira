42fecc4a750c864063270dbe856dc5ee40054d94
Natália Félix <nfelixmacedo@gmail.com>
2024-09-10 15:39:50 -0300

    refactor: atualização na CD

--- pages/api/upload.ts
+++ pages/api/upload.ts
@@ -1,3 +1,4 @@
+/* eslint-disable @typescript-eslint/no-unused-vars */
 import { NextApiHandler, NextApiRequest } from "next";
 import formidable from "formidable";
 import { promises as fs } from "fs";
